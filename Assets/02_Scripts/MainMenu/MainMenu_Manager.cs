c2420bfba16079cc70bc29ac9da7fbdd22863c50
Icloy <70014902+Icloy@users.noreply.github.com>
2023-01-20 23:44:53 +0900

    Ver 0.03
    메인메뉴 버튼들 연결 시켜놓고 이미지 대충 넣어놓음

--- Assets/02_Scripts/MainMenu/MainMenu_Manager.cs
+++ Assets/02_Scripts/MainMenu/MainMenu_Manager.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu_Manager : MonoBehaviour
 {
-    //메인메뉴 버튼들
-    public GameObject exitBtn;
-
-
+    public GameObject optionpanel;
 
 
     void Start()
@@ -19,6 +18,23 @@ public class MainMenu_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GameBtnClick() //Game버튼 클릭시
+    {
+        SceneManager.LoadScene("Loading_Scene"); 
+    }
+
+    public void OptionBtnClick() //Option버튼 클릭시와 Apply버튼 클릭시 
+    {
+        if (optionpanel.activeSelf == true) //옵션패널이 켜져있다면 끄고 꺼져있다면 킨다.
+        {
+            optionpanel.SetActive(false);
+        }
+        else
+        {
+            optionpanel.SetActive(true);
+        }
     }
 
     public void exitBtnClick() // exit버튼 클릭시 게임 종료 처리
