bdd9a4f9a871101cbb617723bf8c39b47cc49def
Icloy <70014902+Icloy@users.noreply.github.com>
2023-02-07 04:32:03 +0900

    Ver 0.16
    GameScene UI 초안작업2

--- Assets/02_Scripts/GameScene/GameManager.cs
+++ Assets/02_Scripts/GameScene/GameManager.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject pausePanel; //게임정지패널
+    public GameObject optionPanel; //게임정지패널
+
 
     void Update()
     {
         //esc가 입력되면 게임을 정지시키고 옵션창을 띄운다.
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            //해당 함수실행
+            if (optionPanel.activeSelf)
+            {
+                optionBtn();
+                return;
+            }
             pasueGame();
         }
     }
@@ -30,4 +37,29 @@ public class GameManager : MonoBehaviour
             pausePanel.SetActive(false);
         }
     }
+
+    public void resumeBtn() //다시 시작
+    {
+        pasueGame();
+    }
+
+    public void optionBtn() //옵션 세부창
+    {
+        if (!optionPanel.activeSelf)
+        {
+            pausePanel.SetActive(false);
+            optionPanel.SetActive(true); 
+        }
+        else 
+        {
+            optionPanel.SetActive(false);
+            pausePanel.SetActive(true);
+        }
+    }
+
+    public void ExitBtn() //메인 화면으로
+    {
+        SceneManager.LoadScene("Menu_Scene");
+    }
+
 }
