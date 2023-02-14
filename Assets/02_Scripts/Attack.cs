a3827cf41991978f6bbb4f7764c768adf2384fe8
Yuhahak <dbwogkr1214@naver.com>
2023-02-14 21:54:47 +0900

    Ver 0.29
    1. e키 누를 시 투사체 발사

---
+++ Assets/02_Scripts/Attack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject bolt;
+    public Transform pos;
+    public float Boltcooltime;
+    private float curtime;
+
+    private AudioSource AudioPlayer; //오디오 소스 컴포넌트
+    public AudioClip BoltAttackSound;
+
+
+    void Awake()
+    {
+        AudioPlayer = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 위치
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+
+
+        if (curtime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(bolt, pos.position, transform.rotation);
+                AudioPlayer.PlayOneShot(BoltAttackSound);
+                curtime = Boltcooltime;
+            }
+        }
+        curtime -= Time.deltaTime;
+    }
+    
+}
