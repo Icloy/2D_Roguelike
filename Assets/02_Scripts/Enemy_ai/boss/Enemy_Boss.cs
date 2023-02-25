2f23af12d967ec290e66ae71981e683472ecf899
unghwls <103508000+unghwls@users.noreply.github.com>
2023-02-25 22:52:30 +0900

    Ver 0.43_2
    에셋 적용후 애니메이션 시간 조정 및 자연스럽게 설정 else_1,else_2

--- Assets/02_Scripts/Enemy_ai/boss/Enemy_Boss.cs
+++ Assets/02_Scripts/Enemy_ai/boss/Enemy_Boss.cs
@@ -20,11 +20,13 @@ public class Enemy_Boss : MonoBehaviour
     private int direction;
     private int dropcnt;
     private int dropran;
+    private bool flag;
     string animationState = "animationState";
 
     enum States
     {
-        idle = 1
+        idle = 1,
+        cast = 6
     }
 
     void Start()
@@ -33,6 +35,7 @@ public class Enemy_Boss : MonoBehaviour
         circle = GetComponent<CircleCollider2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
+        flag = true;
     }
 
     void Update()
@@ -62,44 +65,22 @@ public class Enemy_Boss : MonoBehaviour
             Debug.Log(actmove);
             if (dis <= 2.5)
             {
-                StartCoroutine(act1(actmove));
+                act1(actmove);
             }
             else if (dis <= 5)
             {
-                StartCoroutine(act2(actmove));
+                act2(actmove);
             }
             else
             {
-                switch (actmove)
-                {
-                    case 1:
-                        Debug.Log("else case1");
-                        for (int i = -2; i < 3; i++)
-                        {
-                            Instantiate(trap, new Vector3(Player.transform.position.x + 1f * i, Player.transform.position.y + 4f, Player.transform.position.z), Quaternion.identity);
-                        }
-                        break;
-                    case 2:
-                        Debug.Log("else case2");
-                        dropcnt = Random.Range(-3, 5);
-                        Debug.Log(dropcnt);
-                        for (int i = -3; i <= dropcnt; i++)
-                        {
-                            dropran = Random.Range(1, 6);
-                            if(dropran <= 4)
-                            {
-                                Instantiate(drop, new Vector3(Player.transform.position.x +1f * i, Player.transform.position.y + 4f, Player.transform.position.z), Quaternion.identity);
-                            }
-                        }
-                        break;
-                }
+                StartCoroutine(act3(actmove));
             }
-            yield return new WaitForSeconds(4f);
+            yield return new WaitForSeconds(5f);
         }
-       
+        
     }
 
-    public IEnumerator act1(int actmove)
+    void act1(int actmove)
     {
         Debug.Log("act1");
         switch (actmove)
@@ -109,10 +90,9 @@ public class Enemy_Boss : MonoBehaviour
             case 2:
                 break;
         }
-        yield return new WaitForSeconds(3f);
     }
 
-    public IEnumerator act2(int actmove)
+    void act2(int actmove)
     {
         Debug.Log("act2");
         switch (actmove)
@@ -141,15 +121,51 @@ public class Enemy_Boss : MonoBehaviour
                 }
                 break;
         }
-        yield return new WaitForSeconds(3f);
+    }
+
+    public IEnumerator act3(int actmove)
+    {
+        Debug.Log("act3");
+        animator.SetInteger(animationState, (int)States.cast);
+        yield return new WaitForSeconds(2.5f);
+        switch (actmove)
+        {
+            case 1:
+                Debug.Log("else case1");
+                for (int i = -2; i < 3; i++)
+                {
+                    Instantiate(trap, new Vector3(Player.transform.position.x + 1f * i, Player.transform.position.y + 4f, Player.transform.position.z), Quaternion.identity);
+                }
+                break;
+            case 2:
+                Debug.Log("else case2");
+
+                dropcnt = Random.Range(-3, 5);
+                Debug.Log(dropcnt);
+                for (int i = -3; i <= dropcnt; i++)
+                {
+                    dropran = Random.Range(1, 6);
+                    if (dropran <= 4)
+                    {
+                        Instantiate(drop, new Vector3(Player.transform.position.x + 1f * i, Player.transform.position.y + 2.5f, Player.transform.position.z), Quaternion.identity);
+                    }
+                }
+                break;
+        }
+        animator.SetInteger(animationState, (int)States.idle);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (flag == true)
         {
-            StartCoroutine(Think());
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                StartCoroutine(Think());
+                flag = false;
+            }
         }
+
     }
 
 }
--- Assets/02_Scripts/Enemy_ai/boss/Summon_2.cs
+++ Assets/02_Scripts/Enemy_ai/boss/Summon_2.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class Summon_2 : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator animator;
+    string animationState = "animationState";
+
+    enum States
+    {
+        idle = 1,
+        atk = 2
+    }
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        animator.SetInteger(animationState, (int)States.idle);
+        Invoke("act", 1f);
     }
 
+    void act()
+    {
+        animator.SetInteger(animationState, (int)States.atk);
+        Destroy(this.gameObject, 3.5f);
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Platform"))
         {
-            Destroy(this.gameObject);
+            
         }
     }
 }
