ef5ee3941614fea9a3d82205b52f7a226e77ac4a
Icloy <70014902+Icloy@users.noreply.github.com>
2023-03-04 22:01:59 +0900

    Hp Status 관련된 스프라이트들
    ver < 이거 다 따로 노는데 제목으로 커밋하자구
    Hp Status와 관련된 스프라이트 추가
    관련된 애니메이션 코드 제작 중

--- Assets/02_Scripts/GameScene/GameUI.cs
+++ Assets/02_Scripts/GameScene/GameUI.cs
@@ -5,5 +5,18 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
+    public Image emptyHeart; //빈하트
+    public Image staticHeart; //평범한 하트
+
+
+
+
+    //데미지 받으면 애니메이션 재생 후 하트지운다.
+
+    //일정 시간마다 애니메이션 재생
+
+
+
+
 
 }
