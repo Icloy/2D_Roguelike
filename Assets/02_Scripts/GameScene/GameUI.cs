71e3eb29d7732797637d3646c6ecb3fa31552160
Icloy <70014902+Icloy@users.noreply.github.com>
2023-03-06 22:17:04 +0900

    마우스 커서
    관련 소스 업데이트
    기본작업

--- Assets/02_Scripts/GameScene/GameUI.cs
+++ Assets/02_Scripts/GameScene/GameUI.cs
@@ -5,5 +5,12 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
+    public Texture2D cursor;    //커서 이미지
+    public CursorMode cm = CursorMode.Auto; //커서의 작동 방식
+    public Vector2 cpos = Vector2.zero; //마우스 포인트의 위치
 
+    private void Start()
+    {
+        Cursor.SetCursor(cursor, cpos, cm);
+    }   
 }
