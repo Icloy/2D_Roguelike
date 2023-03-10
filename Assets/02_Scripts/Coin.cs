04722b1b923e6eb8600958e11f09841518dd7489
Icloy <70014902+Icloy@users.noreply.github.com>
2023-03-10 21:33:50 +0900

    코인
    일단 기능은 하게 바꿔놓음
    반전문제는 부모객체를 그냥 카메라로 바꿈

--- Assets/02_Scripts/Coin.cs
+++ Assets/02_Scripts/Coin.cs
@@ -5,34 +5,26 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     GameManager gameManager;
-    GameObject target;
-    Collider coll;
-    Player player;
+    public GameObject target;
+    public CircleCollider2D circle;
 
-    public float updistance;
-
-    public float speed;
-    public float position_change_second;
-
-    Coroutine movecoroutine;
-    CircleCollider2D circle;
-    Transform targetTransform = null;
-    Vector3 position;
-    Rigidbody2D rigid;
+    float speed;
+    Vector3 startPos;
 
     private void Awake()
     {
         gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
         target = GameObject.Find("coincollect");
+        circle = GetComponent<CircleCollider2D>();
     }
 
     private void Start()
     {
-        circle = GetComponent<CircleCollider2D>();
-        rigid = GetComponent<Rigidbody2D>();
-        rigid.AddForce(Vector2.up * updistance, ForceMode2D.Impulse);
+        speed = 13f;
+        startPos = this.transform.position;
     }
 
+/*
     public IEnumerator Move(Rigidbody2D rigidBodyToMove, float speed)
     {
         float remaindistance = (transform.position - position).sqrMagnitude;
@@ -51,42 +43,34 @@ public class Coin : MonoBehaviour
             yield return new WaitForFixedUpdate();
         }
     }
+*/
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (collision.gameObject.tag == "Player")
+        if (col.gameObject.CompareTag("Player"))
         {
-            //targetTransform = collision.gameObject.transform;
-            targetTransform = target.transform;
-            StartCoroutine(Move(rigid, speed));
+            circle.enabled = false;
+            Vector3 pos = col.transform.position;
+            StartCoroutine(CoinMove());
         }
-    }*/
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            rigid.isKinematic = true;
-            circle.enabled = true;
-            targetTransform = target.transform;
-            StartCoroutine(Move(rigid, speed));
-        }
-        if (collision.gameObject.CompareTag("UI"))
-        {
-            //코인계수 업데이트
-
-        }
     }
 
-    IEnumerator CoinMove()
+    IEnumerator CoinMove( )
     {
-        //거리로 무한루프 돌리구
-        float distance = (transform.position - target.transform.position).sqrMagnitude;
-        while(distance >= 2f)
+        while(true)
         {
-            gameManager.UpdateCoinCnt(1);
-            Destroy(this.gameObject);
-            //특정거리보다 좁아졌을때 파괴
+            float distance = (target.transform.position - this.transform.position).sqrMagnitude;
+            if(distance >= 1f)
+            {
+                transform.position = Vector3.Lerp(this.transform.position, target.transform.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                gameManager.UpdateCoinCnt(1);
+                Destroy(this.gameObject);
+                break;
+            }
             yield return null;
         }
     }
