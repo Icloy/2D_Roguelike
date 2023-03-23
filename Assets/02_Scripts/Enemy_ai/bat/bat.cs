d6a637360de65a754711c4ec2073b57a22af041e
unghwls <103508000+unghwls@users.noreply.github.com>
2023-03-23 22:47:45 +0900

    몹 스프라이트,프리팹,애니메이션 정리
    정리 및 박쥐 끝

---
+++ Assets/02_Scripts/Enemy_ai/bat/bat.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bat : Enemy
+{
+    public float movespeed;
+    public float position_change_second;
+    public float delete_time;
+    public float knockbackdis;
+
+
+    private float dis;
+    private bool traceflag;
+    private bool Collision_Damage_Delay;
+    CircleCollider2D circle;
+    Rigidbody2D rigid;
+    Transform targetTransform = null;
+    Vector3 position;
+    Animator animator;
+    SpriteRenderer sprite;
+    Coroutine coroutine;
+
+    string animationState = "animationState";
+
+    enum States
+    {
+        idle = 0,
+        fly = 1,
+        hit = 2
+    }
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponentInChildren<CircleCollider2D>();
+        animator = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void start()
+    {
+        traceflag = Collision_Damage_Delay = false;
+        animator.SetInteger(animationState, (int)States.idle);
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(rigid.position, position, Color.red);
+    }
+
+    public IEnumerator Move(Rigidbody2D rigidBodyToMove, float movespeed)
+    {
+        float remaindistance = (transform.position - position).sqrMagnitude;
+        while (remaindistance > float.Epsilon)
+        {
+            animator.SetInteger(animationState, (int)States.fly);
+            Debug.Log(dis);
+            if (targetTransform != null)
+            {
+                position = targetTransform.position;
+            }
+            if (rigidBodyToMove != null)
+            {
+                if (targetTransform.position.x < rigid.transform.position.x)
+                {
+                    sprite.flipX = false;
+                }
+                else
+                {
+                    sprite.flipX = true;
+                }
+                if (traceflag == true)
+                {
+                    Vector3 newposition = Vector3.MoveTowards(rigidBodyToMove.position, position, -movespeed * Time.deltaTime);
+                    rigid.MovePosition(newposition);
+                    remaindistance = (transform.position - position).sqrMagnitude;
+                }
+                else
+                {
+                    Vector3 newposition = Vector3.MoveTowards(rigidBodyToMove.position, position, movespeed * 2 * Time.deltaTime);
+                    rigid.MovePosition(newposition);
+                    remaindistance = (transform.position - position).sqrMagnitude;
+                }
+            }
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public IEnumerator KnockBack()
+    {
+        if (targetTransform != null)
+        {
+            animator.SetInteger(animationState, (int)States.hit);
+            Debug.Log("knockback check");
+            if (targetTransform.transform.position.x < rigid.transform.position.x)
+            {
+                rigid.AddForce(Vector2.right * knockbackdis, ForceMode2D.Impulse);
+            }
+            else
+            {
+                rigid.AddForce(Vector2.left * knockbackdis, ForceMode2D.Impulse);
+            }
+            yield return new WaitForSeconds(1.2f);
+            MoveCall();
+            yield break;
+        }
+    }
+
+    public override void TakeDamage(int AtDmg)
+    {
+        Hp = Hp - AtDmg;
+        Debug.Log(Hp);
+        StopAllCoroutines();
+        StartCoroutine(KnockBack());
+        if (Hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        DropItem();
+        Destroy(this.gameObject);
+    }
+
+    void DropItem()
+    {
+        Debug.Log("호출");
+        for (int i = 0; i < dropcoincnt; i++)
+        {
+            float x = Random.Range(-1f, 1f); // x축 위치 랜덤 설정
+            float y = Random.Range(-1f, 1f); // y축 위치 랜덤 설정
+            Vector2 position = new Vector2(transform.position.x + x, transform.position.y + y);
+            Instantiate(Item, position, Quaternion.identity);
+        }
+    }
+
+    void MoveCall()
+    {
+        coroutine = StartCoroutine(Move(rigid, movespeed));
+    }
+
+    void MoveStop()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("enter");
+            targetTransform = collision.gameObject.transform;
+            MoveCall();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            traceflag = false;
+            if (Collision_Damage_Delay == true)
+            {
+                Collision_Damage_Delay = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Collision_Damage_Delay == false)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                Collision_Damage_Delay = true;
+                Debug.Log("false");
+                traceflag = true;
+                Player.instance.Damaged(-collision_damage);
+            }
+        }
+    }
+}
