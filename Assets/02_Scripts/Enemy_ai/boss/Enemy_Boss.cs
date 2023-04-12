84ac671b0cd13d13a840a3ba89e71943da3686e0
unghwls <103508000+unghwls@users.noreply.github.com>
2023-04-12 16:51:42 +0900

    기존에 보스로 쓰려던애 몹으로 바꾸는중
    필요한 컴포넌트 추가 관련 코드 수정(애니메이션 아직 확인 안함), 수정하면서 디버그

--- Assets/02_Scripts/Enemy_ai/boss/Enemy_Boss.cs
+++ Assets/02_Scripts/Enemy_ai/boss/Enemy_Boss.cs
@@ -8,26 +8,35 @@ public class Enemy_Boss : Enemy
     Rigidbody2D rigid;
     SpriteRenderer sprite;
     Animator animator;
+    Coroutine thinkcoroutine;
 
     public float jumpPower;
-    public GameObject Player;
-    public GameObject Boss;
     public GameObject trap;
     public GameObject drop;
 
+    public float tracespeed;
+    public float turnrange;
+
+    private bool trace;
+    private bool turnflag;
+    bool bloodflag;
+    private int nextMove;
     private int actmove;
     private int direction;
-    private int dropcnt;
     private int dropran;
+    private int dropcnt;
     string animationState = "animationState";
 
     public GameObject Attack1_check;
     public GameObject Attack2_check;
-
+    public GameObject alert;
     enum States
     {
         idle = 1,
+        walk = 2,
+        hit = 3,
         attack = 4,
+        die = 5,
         cast = 6,
         disappear = 7,
         appears = 8,
@@ -40,8 +49,7 @@ public class Enemy_Boss : Enemy
         circle = GetComponentInChildren<CircleCollider2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
-        Player = GameObject.FindWithTag("Player");
-        StartCoroutine(Think());
+        StartCoroutine(AttackThink());
     }
 
     void Update()
@@ -52,10 +60,78 @@ public class Enemy_Boss : Enemy
     {
         while (true)
         {
+            float nextThinkTime = Random.Range(2f, 5f); // 대기시간 2초에서 5초 사이랜덤
+            if (turnflag == true)
+            {
+                yield return new WaitForSeconds(nextThinkTime);
+            }
+            nextMove = Random.Range(-1, 2); // -1~1 랜덤돌리고 값 던지기
+            yield return new WaitForSeconds(nextThinkTime);
+        }
+    }
+
+    public IEnumerator move()
+    {
+        while (true)
+        {
+            if (PlayerPos != null)
+            {
+                dis = Vector2.Distance(PlayerPos.transform.position, rigid.transform.position);
+                rigid.velocity = Vector2.zero;
+                yield return StartCoroutine(AttackThink());
+            }
+            if (nextMove == 1)//애니메이션 및 스프라이트 방향 변경 자식 개체까지 바꿔야하므로 가장 상위 transform 변경
+            {
+                FlipBack();
+                animator.SetInteger(animationState, (int)States.walk);
+            }
+            else if (nextMove == -1)
+            {
+                FlipX();
+                animator.SetInteger(animationState, (int)States.walk);
+            }
+            else if (nextMove == 0)
+            {
+                animator.SetInteger(animationState, (int)States.idle);
+            }
+            if (trace == true)//추적
+            {
+                if (PlayerPos.transform.position.x < rigid.transform.position.x)
+                {
+                    FlipX();
+                    nextMove = -1;
+                    rigid.velocity = new Vector2(nextMove * tracespeed, rigid.velocity.y);
+                    animator.SetInteger(animationState, (int)States.walk);
+                }
+                else
+                {
+                    FlipBack();
+                    nextMove = 1;
+                    rigid.velocity = new Vector2(nextMove * tracespeed, rigid.velocity.y);
+                    animator.SetInteger(animationState, (int)States.walk);
+                }
+            }
+            if (trace == false) //땅 체크 + 턴 및 이동
+            {
+                Vector2 frontVec = new Vector2(rigid.position.x + nextMove * turnrange, rigid.position.y);
+                Debug.DrawRay(frontVec, Vector3.down, new Color(0, 255, 0));
+                RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 2, LayerMask.GetMask("Platform"));
+                if (rayHit.collider == null)
+                {
+                    Turn();
+                }
+                rigid.velocity = new Vector2(nextMove * movespeed, rigid.velocity.y);
+            }
+            yield return new WaitForSeconds(0f);
+        }
+    }
+
+    public IEnumerator AttackThink()
+    {
             animator.SetInteger(animationState, (int)States.idle);
-            float dis = Vector2.Distance(Player.transform.position, Boss.transform.position);
-            Vector2 player = Player.transform.position;
-            if (player.x < Boss.transform.position.x)
+            dis = Vector2.Distance(PlayerPos.transform.position, rigid.transform.position);
+            Vector2 player = PlayerPos.transform.position;
+            if (player.x < rigid.transform.position.x)
             {
                 direction = 1;
                 FlipBack();
@@ -81,9 +157,6 @@ public class Enemy_Boss : Enemy
             {
                 yield return StartCoroutine(act3(actmove));
             }
-            yield return new WaitForSeconds(0f);
-        }
-        
     }
 
     public IEnumerator act1(int actmove)
@@ -128,7 +201,7 @@ public class Enemy_Boss : Enemy
                 switch (j)
                 {
                     case 1:
-                        gameObject.transform.position = new Vector3(Player.transform.position.x + 1.5f, Player.transform.position.y + 5f, Player.transform.position.z);
+                        gameObject.transform.position = new Vector3(PlayerPos.transform.position.x + 1.5f, PlayerPos.transform.position.y + 5f, PlayerPos.transform.position.z);
                         FlipBack();
                         animator.SetInteger(animationState, (int)States.appears);
                         yield return new WaitForSeconds(1f);
@@ -138,7 +211,7 @@ public class Enemy_Boss : Enemy
                         yield return new WaitForSeconds(1f);
                         break;
                     case 2:
-                        gameObject.transform.position = new Vector3(Player.transform.position.x + -1.5f, Player.transform.position.y + 5f, Player.transform.position.z);
+                        gameObject.transform.position = new Vector3(PlayerPos.transform.position.x - 1.5f, PlayerPos.transform.position.y + 5f, PlayerPos.transform.position.z);
                         FlipX();
                         animator.SetInteger(animationState, (int)States.appears);
                         yield return new WaitForSeconds(1f);
@@ -157,7 +230,7 @@ public class Enemy_Boss : Enemy
                 switch (i)
                 {
                     case 1:
-                        gameObject.transform.position = new Vector3(Player.transform.position.x + 1.5f, Player.transform.position.y-0.5f, Player.transform.position.z);
+                        gameObject.transform.position = new Vector3(PlayerPos.transform.position.x + 1.5f, PlayerPos.transform.position.y - 0.5f, PlayerPos.transform.position.z);
                         FlipBack();
                         animator.SetInteger(animationState, (int)States.appears);
                         yield return new WaitForSeconds(1f);
@@ -166,7 +239,7 @@ public class Enemy_Boss : Enemy
                         animator.SetInteger(animationState, (int)States.idle);
                         break;
                     case 2:
-                        gameObject.transform.position = new Vector3(Player.transform.position.x + -1.5f, Player.transform.position.y-0.5f, Player.transform.position.z);
+                        gameObject.transform.position = new Vector3(PlayerPos.transform.position.x -1.5f, PlayerPos.transform.position.y-0.5f, PlayerPos.transform.position.z);
                         FlipX();
                         animator.SetInteger(animationState, (int)States.appears);
                         yield return new WaitForSeconds(1f);
@@ -190,11 +263,11 @@ public class Enemy_Boss : Enemy
                 Debug.Log("act3_1");
                 if(direction == 1)
                 {
-                        Instantiate(trap, new Vector3(Boss.transform.position.x + -2f, Boss.transform.position.y + 1f, Boss.transform.position.z), Quaternion.identity);
+                    Instantiate(trap, new Vector3(rigid.transform.position.x - 2f, rigid.transform.position.y + 1f, rigid.transform.position.z), Quaternion.identity);
                 }
                 else
                 {
-                        Instantiate(trap, new Vector3(Boss.transform.position.x + 2f, Boss.transform.position.y + 1f, Boss.transform.position.z), Quaternion.identity);
+                    Instantiate(trap, new Vector3(rigid.transform.position.x + 2f, rigid.transform.position.y + 1f, rigid.transform.position.z), Quaternion.identity);
                 }
                 yield return new WaitForSeconds(1.65f);
 
@@ -209,12 +282,72 @@ public class Enemy_Boss : Enemy
                     dropran = Random.Range(1, 6);
                     if (dropran <= 4)
                     {
-                        Instantiate(drop, new Vector3(Player.transform.position.x + 1f * i, Player.transform.position.y + 2.5f, Player.transform.position.z), Quaternion.identity);
+                        Instantiate(drop, new Vector3(PlayerPos.transform.position.x + 1f * i, PlayerPos.transform.position.y + 2.5f, PlayerPos.transform.position.z), Quaternion.identity);
                     }
                 }
                 break;
         }
         animator.SetInteger(animationState, (int)States.idle);
+    }
+
+    public IEnumerator KnockBack()
+    {
+        if (PlayerPos != null)
+        {
+            rigid.velocity = Vector2.zero;
+            if (bloodflag == true)
+            {
+                StartCoroutine(LBlood());
+            }
+            else
+            {
+                StartCoroutine(RBlood());
+            }
+            animator.SetInteger(animationState, (int)States.hit);
+            rigid.AddForce(Vector2.up * knockbackdis, ForceMode2D.Impulse);
+            if (PlayerPos.transform.position.x < rigid.transform.position.x)
+            {
+                rigid.AddForce(Vector2.right * knockbackdis, ForceMode2D.Impulse);
+            }
+            else
+            {
+                rigid.AddForce(Vector2.left * knockbackdis, ForceMode2D.Impulse);
+            }
+            yield return new WaitForSeconds(1.2f);
+            StartCoroutine(move());
+            ThinkCall();
+            yield break;
+        }
+    }
+
+    IEnumerator Alert()
+    {
+        alert.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        alert.gameObject.SetActive(false);
+    }
+
+    IEnumerator LBlood()
+    {
+        lblood.Play();
+        yield return new WaitForSeconds(0.6f);
+        lblood.Stop();
+    }
+
+    IEnumerator RBlood()
+    {
+        rblood.Play();
+        yield return new WaitForSeconds(0.6f);
+        rblood.Stop();
+    }
+
+    IEnumerator Die()
+    {
+        rigid.velocity = Vector2.zero;
+        animator.SetInteger(animationState, (int)States.die);
+        yield return new WaitForSeconds(0.2f);
+        DropItem();
+        Destroy(this.gameObject);
     }
 
     public override void TakeDamage(int AtDmg)
@@ -223,15 +356,35 @@ public class Enemy_Boss : Enemy
         Debug.Log(Hp);
         if (Hp <= 0)
         {
-            Die();
+            StopAllCoroutines();
+            StartCoroutine(Die());
         }
-
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(KnockBack());
+        }
     }
 
-    void Die()
+    void Turn()//턴
     {
-        DropItem();
-        Destroy(this.gameObject);
+        nextMove = nextMove * (-1);
+        turnflag = true;
+        ThinkStop();
+        ThinkCall();
+    }
+
+    void ThinkCall()
+    {
+        thinkcoroutine = StartCoroutine(Think());
+    }
+
+    void ThinkStop()
+    {
+        if (thinkcoroutine != null)
+        {
+            StopCoroutine(thinkcoroutine);
+        }
     }
 
     void DropItem()
@@ -253,5 +406,32 @@ public class Enemy_Boss : Enemy
     void FlipBack()
     {
         transform.localScale = new Vector3(4, 5, 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) //Enter에서 Stay로 변경 공격을 당하면 자식트리거쪽 exit도 반응해서 추적 풀려버림
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Alert());
+            if (PlayerPos == null)
+            {
+                PlayerPos = collision.gameObject.transform;
+            }
+            trace = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player exit");
+            if (PlayerPos != null)
+            {
+                PlayerPos = null;
+            }
+            trace = false;
+        }
     }
 }
