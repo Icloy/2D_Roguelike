06d4bbfdace62caed6eb26a4f8e4da353a2bf2f9
Yuhahak <dbwogkr1214@naver.com>
2023-04-15 23:20:30 +0900

    맵 중간 커밋

---
+++ Assets/02_Scripts/Player/Camera/BackGround/GrassAnim.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetBool("Move",true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetBool("Move", false);
+        }
+    }
+
+}
