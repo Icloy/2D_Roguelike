29734677c1072d7c5542aad9b8038f15041170fc
unghwls <103508000+unghwls@users.noreply.github.com>
2023-04-19 20:16:43 +0900

    공격시 반동 중복호출 막음

--- Assets/02_Scripts/Player/AttackBound.cs
+++ Assets/02_Scripts/Player/AttackBound.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class AttackBound : MonoBehaviour
 {
     Rigidbody2D rigid;
+    bool platformrepeat;
+    bool enemyrepeat;
 
-    void Start()
+    void OnEnable()
     {
+        enemyrepeat = platformrepeat = false;
         rigid = GetComponentInParent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Platform") || collision.gameObject.CompareTag("Enemy"))
+        if (collision.CompareTag("Platform"))
         {
-            rigid.velocity = Vector2.zero;
-            Player.Instance.upforce();
+            if(!platformrepeat && !enemyrepeat)
+            {
+                platformrepeat = true;
+                rigid.velocity = Vector2.zero;
+                Player.Instance.upforce();
+            }
+        }
+        else if (collision.CompareTag("Enemy"))
+        {
+            if (!platformrepeat && !enemyrepeat)
+            {
+                enemyrepeat = true;
+                rigid.velocity = Vector2.zero;
+                Player.Instance.upforce();
+            }
         }
     }
-
-
-
 }
--- Assets/02_Scripts/Player/AttackBoundSide.cs
+++ Assets/02_Scripts/Player/AttackBoundSide.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class AttackBoundSide : MonoBehaviour
 {
     Rigidbody2D rigid;
+    bool platformrepeat;
+    bool enemyrepeat;
 
-    void Start()
+    void OnEnable()
     {
+        enemyrepeat = platformrepeat = false;
         rigid = GetComponentInParent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Platform") || collision.gameObject.CompareTag("Enemy"))
+        if (collision.CompareTag("Platform"))
         {
-            rigid.velocity = Vector2.zero;
-            Player.Instance.sideForce();
+            if (!platformrepeat && !enemyrepeat)
+            {
+                platformrepeat = true;
+                rigid.velocity = Vector2.zero;
+                Player.Instance.upforce();
+            }
+        }
+        else if (collision.CompareTag("Enemy"))
+        {
+            if (!platformrepeat && !enemyrepeat)
+            {
+                enemyrepeat = true;
+                rigid.velocity = Vector2.zero;
+                Player.Instance.upforce();
+            }
         }
     }
 }
