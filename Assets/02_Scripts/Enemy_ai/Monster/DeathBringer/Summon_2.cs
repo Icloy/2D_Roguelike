f0447ca8ba8f3d85d968f6fcbab0572cb725c5c2
unghwls <103508000+unghwls@users.noreply.github.com>
2023-04-20 20:26:13 +0900

    sprite,animation 정리 패턴 코드수정

--- Assets/02_Scripts/Enemy_ai/Monster/DeathBringer/Summon_2.cs
+++ Assets/02_Scripts/Enemy_ai/Monster/DeathBringer/Summon_2.cs
@@ -6,6 +6,8 @@ public class Summon_2 : MonoBehaviour
 {
     Rigidbody2D rigid;
     Animator animator;
+    bool dmgrepeat;
+    public int damage;
     string animationState = "animationState";
 
     enum States
@@ -19,19 +21,31 @@ public class Summon_2 : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
         animator.SetInteger(animationState, (int)States.idle);
+        dmgrepeat = true;
         Invoke("act", 1f);
     }
 
     void act()
     {
         animator.SetInteger(animationState, (int)States.atk);
+        Invoke("damamgetrue", 1f);
         Destroy(this.gameObject, 3.5f);
     }
-    void OnCollisionEnter2D(Collision2D collision)
+
+    void damagetrue()
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Platform"))
+        dmgrepeat = false;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
-            
+            if(!dmgrepeat)
+            {
+                dmgrepeat = true;
+                Player.instance.Damaged(-damage);
+            }
         }
     }
 }
