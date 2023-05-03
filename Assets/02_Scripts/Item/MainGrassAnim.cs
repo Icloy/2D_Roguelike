aebc61af7ffcafaf0fea167701ad81524dd929e6
Yuhahak <dbwogkr1214@naver.com>
2023-05-03 18:39:23 +0900

    맵 중간 커밋
    떨어지자마자 있는 텐트는 상점
    오른쪽 우물로 내려가면 던전

--- Assets/02_Scripts/Item/MainGrassAnim.cs
+++ Assets/02_Scripts/Item/MainGrassAnim.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class MainGrassAnim : Breakable
 {
     [SerializeField] private AudioClip grassMove;
-    [SerializeField] private AudioClip grassCut;
-    [SerializeField] private GameObject grassAlive;
-    [SerializeField] private GameObject grassDeadParticle;
 
     private Animator anim;
     private AudioSource audioSource;
@@ -18,10 +15,6 @@ public class MainGrassAnim : Breakable
         anim = GetComponentInChildren<Animator>();
 
     }
-    private void Update()
-    {
-        CheckIsDead();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -29,29 +22,9 @@ public class MainGrassAnim : Breakable
             return;
         if (collision.gameObject.CompareTag("Player"))
         {
-            Vector3 diff = (collision.transform.position - transform.position).normalized;
-            if (diff.x > 0)
-            {
-                anim.SetTrigger("GrassRight");
-            }
-            else if (diff.x < 0)
-            {
-                anim.SetTrigger("GrassLeft");
-            }
             audioSource.PlayOneShot(grassMove);
         }
 
-        if (collision.gameObject.CompareTag("AttackZone"))
-        {
-            Hurt(1);
-        }
     }
 
-    protected override void Dead()
-    {
-        base.Dead();
-        audioSource.PlayOneShot(grassCut);
-        grassDeadParticle.SetActive(true);
-        grassAlive.SetActive(false);
-    }
 }
